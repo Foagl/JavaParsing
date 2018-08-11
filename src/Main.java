b8f468e9eb8f212401f0b3785eb6154120cdd8a0
Valer <Valeris2812@mail.ru>
2018-08-11 22:49:05 +0200

    Работает парсинг, считывание информацыи, вывод её в String, работа через командную строку

--- src/Main.java
+++ src/Main.java
@@ -8,56 +8,77 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
-class Article {
-    @Override
-    public String toString() {
-        return "Article{" +
-                "url='" + url + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
 
-    public Article(String url, String name) {
-        this.url = url;
-        this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    private String url;
-    private String name;
-}
 public class Main {
+    private static  Double FilterStringForNumber (Object str){
+        String sum =  str.toString();
+        String result = sum.replaceAll("[a-zA-Zа > < /]*", "");
+        double var2 = new Double(result);
+        return var2;
+    }
+    private static  String FilterStringForName (Object str){
+        String sum =  str.toString();
+        String result = sum.replaceAll("[zA-Za 0-9 t d (srongsrong)> < / ]*", "");
+//        String result2 = result.replaceAll(">", "");
+//        String result3 = result2.replaceAll("<", "");
+//        String result4 = result3.replaceAll("/", "");
+        return result;
+    }
 
 
     public static void main(String[] args) throws IOException {
-        List<Article> ArticleList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+            int i = 0;
 
-        Document doc = Jsoup.connect("http://4pda.ru/").get();
-        Elements h1Elements = doc.getElementsByAttributeValue("class", "list-post-title");
+            List text = new ArrayList();
+            Document doc = Jsoup.connect("https://zlata.ws/kantory/").get();
+            Elements elements = doc.select("td").select("td");
+                //Elements Cource = doc.attr("span");
+             for ( Element element: elements ){
+                 text.add(element);
+                 i++;
+                    //System.out.println(element);
+             };
+                    //System.out.println(i); // - How much string on list list "text'.
+//                System.out.println(FilterStringForName(text.get(135)));
+//                System.out.println(FilterStringForNumber(text.get(136)));
+//
+//                System.out.println(FilterStringForName(text.get(138)));
+//                System.out.println(FilterStringForNumber(text.get(139)));
+            double operand1 = FilterStringForNumber(text.get(136)); // гривна
+            double operand2 = FilterStringForNumber(text.get(139)); // Доллар
+             System.out.println("Какую валюту нужно переводить?: 1. Гривна 2. Злотый 3 Доллар :");
+              int select = scanner.nextInt();
 
-        h1Elements.forEach(h1Element ->{
-              Element aElements = h1Element.child(0);
-              String url = aElements.attr("href");
-              String title = aElements.child(0).text();
-            ArticleList.add(new Article(url, title));
-        });
-               ArticleList.forEach(System.out::println);
+                if (select == 1){
+            System.out.print("Злотый к гривне > ");
+            double operand3 = scanner.nextDouble();
+            operand3 = operand3 * operand1;
+            System.out.println("> " + operand3);
+        }
+             if ( select == 2 ){
+                 System.out.print(" Гривна к доллару > ");
+                 double operand3 = scanner.nextDouble();
+                 operand3 = (operand3 * operand1) / operand2; // Расчет гривны к доллару   Sum * KursUAH / KursUSD;
+                 System.out.println("> " + operand3);
+             }
+
+             if (select == 3){
+                 System.out.print("Злотый к Доллару > ");
+                 double operand3 = scanner.nextDouble();
+                 operand3 = operand3 / operand1; // Расчет гривны к доллару   Sum * KursUAH / KursUSD;
+                 System.out.println("> " + operand3);
+             }
+
+
+
+
+
+
+
     }}
 
 
